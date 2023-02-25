0b8a202be69c6bf5b2d730b60c16f098d3e4ea90
CapyBJ <bharadwaj1878@gmail.com>
2023-02-25 23:49:29 +0530

    changes to CORS

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors');
 require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+//
+
+
 // Connect to database
 connectDB()
 
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(cors())
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
@@ -22,7 +29,6 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
   app.get('/', (_, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
-
 
 app.use(errorHandler)
 
